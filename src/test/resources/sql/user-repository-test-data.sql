226fb54bb02824989b884fa57a259109f86c4aa5
정준영 <jeongjy@mrblue.com>
2023-11-13 10:30:08 +0900

    test: repository test codd add

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,1 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`) values (1, 'jeongjy620@gmail.com', 'jeongjy', 'seoul', 'aaaa-aaaaa-aaaaaa', 'ACTIVE', 0)
