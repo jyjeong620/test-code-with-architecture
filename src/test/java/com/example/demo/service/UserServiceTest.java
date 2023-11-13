3964eabb1cf295e0ac1a4d0760a76f45a4de8837
정준영 <jeongjy@mrblue.com>
2023-11-13 11:28:16 +0900

    test: userService test codd add

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,17 +1,25 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.CertificationCodeNotMatchedException;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
 import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:test-application.properties")
@@ -23,6 +31,8 @@ class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    @MockBean
+    private JavaMailSender mailSender;
 
     @Test
     void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다() {
@@ -66,5 +76,76 @@ class UserServiceTest {
         assertThatThrownBy(() -> {
             UserEntity result = userService.getById(2L);
         }).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void userCreateDto_를_이용하여_유저를_생성할_수_있다() {
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("jeongjy620@naver.com")
+                .address("Gyeonggi")
+                .nickname("jeongjy-n")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        // when
+        UserEntity result = userService.create(userCreateDto);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
+    }
+
+    @Test
+    void userUpdateDto_를_이용하여_유저를_수정할_수_있다() {
+        // given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .address("Incheon")
+                .nickname("jeongjy-n")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        // when
+        userService.update(1L, userUpdateDto);
+
+        // then
+        UserEntity userEntity = userService.getById(1L);
+        assertThat(userEntity.getId()).isNotNull();
+        assertThat(userEntity.getAddress()).isEqualTo("Incheon");
+        assertThat(userEntity.getNickname()).isEqualTo("jeongjy-n");
+    }
+
+    @Test
+    void 유저를_로그인_시키면_마지막_로그인_시간이_변경된다() {
+        // given
+        // when
+        userService.login(1L);
+
+        // then
+        UserEntity userEntity = userService.getById(1L);
+        assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+//        assertThat(userEntity.getLastLoginAt()).isEqualTo("T.T");
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_인증코드로_ACTIVE_시킬_수_있다() {
+        // given
+        // when
+        userService.verifyEmail(2L,"aaaa-aaaaa-aaaaaa2");
+
+        // then
+        UserEntity userEntity = userService.getById(2L);
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_에러를_던진다() {
+        // given
+        // when
+
+        // then
+        assertThatThrownBy(() -> {
+            userService.verifyEmail(2L,"aaaa-aaaaa-aaaaaa");
+        }).isInstanceOf(CertificationCodeNotMatchedException.class);
     }
 }
--- src/test/resources/sql/user-service-test-data.sql
+++ src/test/resources/sql/user-service-test-data.sql
@@ -1,3 +1,4 @@
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`) values (1, 'jeongjy620@gmail.com', 'jeongjy', 'seoul', 'aaaa-aaaaa-aaaaaa', 'ACTIVE', 0)
-
-insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`) values (2, 'jeongjy630@gmail.com', 'jeongjy', 'seoul', 'aaaa-aaaaa-aaaaaa', 'PENDING', 0)
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'jeongjy620@gmail.com', 'jeongjy', 'seoul', 'aaaa-aaaaa-aaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'jeongjy630@gmail.com', 'jeongjy01', 'seoul', 'aaaa-aaaaa-aaaaaa2', 'PENDING', 0);
