c30a18604511089904834533c4bee784fc3be80e
정준영 <jeongjy@mrblue.com>
2023-11-13 10:46:27 +0900

    test: userService read test code add

---
+++ src/test/resources/sql/user-service-test-data.sql
@@ -0,0 +1,3 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`) values (1, 'jeongjy620@gmail.com', 'jeongjy', 'seoul', 'aaaa-aaaaa-aaaaaa', 'ACTIVE', 0)
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`) values (2, 'jeongjy630@gmail.com', 'jeongjy', 'seoul', 'aaaa-aaaaa-aaaaaa', 'PENDING', 0)
