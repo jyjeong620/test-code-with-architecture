c0777211feabb37bc8b5fb4a970db117d5b89ab1
정준영 <jeongjy@mrblue.com>
2023-11-21 12:03:40 +0900

    test: CertificationServiceTest code add

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다() {
+        //given
+        FakeMailSender mailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(mailSender);
+
+        //when
+        certificationService.send("jeongjy620@gmail.com",1, "aaaa-aaaaa-aaaaaa");
+
+        //then
+        assertThat(mailSender.email).isEqualTo("jeongjy620@gmail.com");
+        assertThat(mailSender.title).isEqualTo("Please certify your email address");
+        assertThat(mailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaa-aaaaa-aaaaaa");
+    }
+}
